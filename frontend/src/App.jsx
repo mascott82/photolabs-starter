f957aa0e8a770904d688a99850f14f55d7e51195
mascott82 <m.a.scott@live.com>
2024-01-08 23:46:31 -0600

    Featuring - Implement HomeRoute.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,29 +5,13 @@ import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopicList from 'components/TopicList';
 import TopNavigation from 'components/TopNavigationBar';
-
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// }
-
-// const photos = new Array(3).fill(sampleDataForPhotoListItem);
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* {photos.map((photo, index) => (
-        <PhotoListItem key={photo.id + index} photo={photo}/>
-      ))} */}
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   )
 }
