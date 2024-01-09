f083bba8027b4b2a42d7d4858849a68c9054dc8e
mascott82 <m.a.scott@live.com>
2024-01-09 13:00:28 -0600

    Featuring - pass photo details.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
@@ -6,15 +6,18 @@ import { useFavContext } from "./FavContext";
 
 const PhotoListItem = (props) => {
   const { handleFav } = useFavContext()
+  const [selectedPhoto, setSelectedPhoto] = useState(null)
 
-  const handleClick = () => {
+  const openModal = (photo) => {
+    setSelectedPhoto(photo)
     props.setDisplayModal(true)
+    console.log(photo)
   }
 
   return (
     <div className="photo-list__item">
       <PhotoFavButton handleFav={handleFav} photoId={props.photo.id}/>
-      <img className="photo-list__image" src = {props.photo.urls.regular} alt={`Photo by ${props.photo.user.name}`}  onClick={handleClick}/>
+      <img className="photo-list__image" src = {props.photo.urls.regular} alt={`Photo by ${props.photo.user.name}`}  onClick={() => openModal(props.photo)}/>
       <div className="photo-list__user-details">
         <div className="photo-list__user-info">
           <div className="photo-list__user-profile-info">
