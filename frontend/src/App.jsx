c21ae9f445104996409823305b612ff32f453072
mascott82 <m.a.scott@live.com>
2024-01-08 19:42:25 -0600

    Featuring - Implement the skeleton of photolabs.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const {id, city, country, imageSource, username, profile} = sampleDataForPhotoListItem
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem id={id} city={city} country={country} imageSource={imageSource} username={username} profile={profile}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
-};
+const PhotoListItem = (props) => {
+  return (
+    <div className="PhotoListItem" key={props.id}>
+      <img src = {props.imageSource} alt={`Photo by ${props.username}`}/>
+      <div className="PhotoDetail">
+        <h2>{props.id}</h2>
+        <p>{`Location: ${props.city}, ${props.country}`}</p>
+        <div className="UserInfo">
+          <img src={props.profile} alt={`Profile of ${props.username}`} />
+          <p>{`Upload by ${props.username}`}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default PhotoListItem;
