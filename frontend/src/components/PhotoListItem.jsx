a9f59ec7280b426b846970dfd63a400c11327c53
mascott82 <m.a.scott@live.com>
2024-01-08 23:21:03 -0600

    Featuring - Add PhotoList component.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,12 +7,12 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
-      <img className="photo-list__image" src = {props.photo.imageSource} alt={`Photo by ${props.photo.username}`}/>
+      <img className="photo-list__image" src = {props.photo.urls.regular} alt={`Photo by ${props.photo.user.name}`}/>
       <div className="photo-list__user-details">
         <div className="photo-list__user-info">
           <div className="photo-list__user-profile-info">
-            <img className="photo-list__user-profile" src={props.photo.profile} alt={`Profile of ${props.photo.username}`} />
-            <div>{props.photo.username}</div>
+            <img className="photo-list__user-profile" src={props.photo.user.profile} alt={`Profile of ${props.photo.user.name}`} />
+            <div>{props.photo.user.name}</div>
           </div>
           <div className="photo-list__user-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</div>
         </div>
