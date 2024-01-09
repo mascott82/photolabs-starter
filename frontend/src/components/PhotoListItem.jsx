7ca5e21bd256de4acd61d913c586067ecdb22958
mascott82 <m.a.scott@live.com>
2024-01-08 20:01:22 -0600

    Bugfixing - add styling to the PhotoListItem.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,11 +5,11 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item" key={props.photo.id}>
-      <img src = {props.photo.imageSource} alt={`Photo by ${props.photo.username}`}/>
+      <img className="photo-list__image" src = {props.photo.imageSource} alt={`Photo by ${props.photo.username}`}/>
       <div className="photo-list__user-details">
         <h2>{props.photo.id}</h2>
-        <p className="photo-list__user-location">{`Location: ${props.photo.location.city}, ${props.photo.location.country}`}</p>
         <div className="photo-list__user-info">
+        <div className="photo-list__user-location">{`Location: ${props.photo.location.city}, ${props.photo.location.country}`}</div>
           <img src={props.photo.profile} alt={`Profile of ${props.photo.username}`} />
           <p>{`Upload by ${props.photo.username}`}</p>
         </div>
