adf7605227459c630b77fce5dfd061db9c88bf81
mascott82 <m.a.scott@live.com>
2024-01-09 21:16:59 -0600

    Featuring - Implement useReducer.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -4,22 +4,38 @@ const initialState = {
   photos: [],
 }
 
-const ADD_FAVORITE = 'ADD_FAVORITE'
-const REMOVE_FAVORITE = 'REMOVE_FAVORITE'
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case ADD_FAVORITE:
+    case ACTIONS.FAV_PHOTO_ADDED:
       return {
         ...state,
         photos: state.photos.map(photo => 
           photo.id === action.payload ? { ...photo, favorite: true } : photo),
       }
-    case REMOVE_FAVORITE:
+    case ACTIONS.FAV_PHOTO_REMOVED:
       return {
         ...state,
         photos: state.photos.map(photo => 
           photo.id === action.payload ? { ...photo, favorite: false } : photo),
+      }
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+      }
+    case ACTIONS.SET_PHOTO_DATA:
+      return {
+        ...state,
+        photos: action.payload,
       }
     default:
       return state
@@ -30,17 +46,27 @@ const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const addFavorite = (photoId) => {
-    dispatch({ type: ADD_FAVORITE, payload: photoId })
+    dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoId })
   }
 
   const removeFavorite = (photoId) => {
-    dispatch({type: REMOVE_FAVORITE, payload: photoId })
+    dispatch({type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoId })
+  }
+
+  const selectedPhoto = (photoId) => {
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photoId })
+  }
+
+  const setPhotoDate = (newPhotoData) => {
+    dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: newPhotoData })
   }
 
   return {
     state,
     addFavorite,
     removeFavorite,
+    selectedPhoto,
+    setPhotoDate
   }
 }
 
